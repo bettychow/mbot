7401ca14aed88214ce8a8c0347222e1f62ed6003
Betty Chow <bettychchow@gmail.com>
2017-12-29 01:44:00 -0800

    Add verification code

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 // Server index page
-app.get("/", function (req, res) {
-  res.send("Deployed!");
-});
+// app.get("/", function (req, res) {
+//   res.send("Deployed!");
+// });
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === "this_is_my_token") {
+  if (req.query["hub.verify_token"] === "verifycode") {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
