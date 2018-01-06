b1f888d8e595e743e31872def7be8db67fbf00ae
Betty Chow <bettychchow@gmail.com>
2018-01-05 21:31:45 -0800

    changes in index.js

--- index.js
+++ index.js
@@ -1,209 +1,243 @@
-'use strict'
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+var db = mongoose.connect("mongodb://heroku_qdhdtqh8:cr4804gtf8u2iijjtkp936cnsq@ds133557.mlab.com:33557/heroku_qdhdtqh8");
+var Movie = require("./models/movie");
 
-
-
-const app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-// Process application/x-www-form-urlencoded
+var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
-
-// Process application/json
 app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
-// Index route
-app.get('/', function (req, res) {
-	res.send('Hello world, I am Kitty');
+// Server index page
+app.get("/", function (req, res) {
+    res.send("Deployed!");
 });
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-
-	if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
-		res.send(req.query['hub.challenge']);
-	}
-	res.send('Error, wrong token');
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+    if (req.query["hub.verify_token"] === 'my_voice_is_my_password_verify_me') {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
+    }
 });
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'));
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+	console.log('bbb', req.body)
+    // Make sure this is a page subscription
+    if (req.body.object === "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function(entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
 });
 
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
 
-	let response;
-
-	// Check if the message contains text
-	if(received_message.text) {
-		response = {
-			"text": `You sent the message: "${received_message}". Now send me an image!`
-		}
-  } else if (received_message.attachments) {
-
-		// Gets the URL of the message attachment
-		let attachment_url = received_message.attachments[0].payload.url;
-		response = {
-			"attachment": {
-				"type": "template",
-				"payload": {
-					"template_type": "generic",
-					"elements": [{
-						"title": "Is this the right picture?",
-						"subtitle": "Tap a button to answer.",
-						"image_url":  attachment_url,
-						"buttons": [
-							{
-								"type": "postback",
-								"title": "Yes!",
-								"payload": "yes",
-							},
-							{
-								"type": "postback",
-								"title": "No!",
-								"payload": "no",
-							}
-						],
-					}]
-				}
-			}
-		}
-	}
-
-// Sends the response message
-callSendAPI(sender_psid, response);
-
+    if (payload === "Greeting") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: "EAAFdfOUojVABAMka3jZAw7ydGBHZBf7gH1xqCYehq3OF3mGtndpsYahQCWPAlraDqLXMaDFIHot45EBI2SBWlWI7XbmXr1sTVNmPy0Lo7Yrkn2dbi7bZA3ySwnZAlezzVOxZAavXZBqDTF80HCW1l5b3T5Ro4Va7aaYs52IOqy9QZDZD",
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function(error, response, body) {
+            var greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " +  error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+            sendMessage(senderId, {text: message});
+        });
+    } else if (payload === "Correct") {
+        sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+    } else if (payload === "Incorrect") {
+        sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
+    }
 }
 
+function processMessage(event) {
+    if (!event.message.is_echo) {
+        var message = event.message;
+        var senderId = event.sender.id;
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
 
-  let payload = received_postback.payload;
+        // You may get a text or attachment but not both
+        if (message.text) {
+            var formattedMsg = message.text.toLowerCase().trim();
 
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + sender_psid,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        var name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding movie detail.
+            // Otherwise search for new movie.
+            switch (formattedMsg) {
+                case "plot":
+                case "date":
+                case "runtime":
+                case "director":
+                case "cast":
+                case "rating":
+                    getMovieDetail(senderId, formattedMsg);
+                    break;
+
+                default:
+                    findMovie(senderId, formattedMsg);
+            }
+        } else if (message.attachments) {
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+        }
+    }
+}
+
+function findMovie(userId, movieTitle) {
+    request("http://www.omdbapi.com/?i=tt3896198&apikey=11ddc4c0&type=movie&t=" + movieTitle, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movieObj = JSON.parse(body);
+            if (movieObj.Response === "True") {
+                var query = {user_id: userId};
+                var update = {
+                    user_id: userId,
+                    title: movieObj.Title,
+                    plot: movieObj.Plot,
+                    date: movieObj.Released,
+                    runtime: movieObj.Runtime,
+                    director: movieObj.Director,
+                    cast: movieObj.Actors,
+                    rating: movieObj.imdbRating,
+                    poster_url:movieObj.Poster
+                };
+                var options = {upsert: true};
+                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+                    if (err) {
+                        console.log("Database error: " + err);
+                    } else {
+                        message = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "generic",
+                                    elements: [{
+                                        title: movieObj.Title,
+                                        subtitle: "Is this the movie you are looking for?",
+                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                                        buttons: [{
+                                            type: "postback",
+                                            title: "Yes",
+                                            payload: "Correct"
+                                        }, {
+                                            type: "postback",
+                                            title: "No",
+                                            payload: "Incorrect"
+                                        }]
+                                    }]
+                                }
+                            }
+                        };
+												console.log('mmmm', message)
+                        sendMessage(userId, message);
+                    }
+                });
+            } else {
+                console.log(movieObj.Error);
+                sendMessage(userId, {text: movieObj.Error});
+            }
+        } else {
+            sendMessage(userId, {text: "Something went wrong. Try again."});
+        }
+    });
+}
+
+function getMovieDetail(userId, field) {
+    Movie.findOne({user_id: userId}, function(err, movie) {
+        if(err) {
+            sendMessage(userId, {text: "Something went wrong. Try again"});
+        } else {
+					console.log('ffff', movie[field]);
+
+					if(field === 'cast') {
+						let cast = movie[field].split(", ");
+						let castData = cast.map(celeb => {
+							return {type: "celeb", name: celeb}
+						});
+						console.log('ddd', castData)
+						message = {
+								attachment: {
+										type: "template",
+										payload: {
+												template_type: "generic",
+												elements: [{
+												    title: "Here is the cast",
+														subtitle: "Whom do you like most?",
+														buttons: []
+												}]
+										}
+								}
+						};
+console.log(message.attachment.payload.elements[0].buttons)
+
+         let buttons = message.attachment.payload.elements[0].buttons
+
+
+         for(let i = 0; i < castData.length; i++) {
+					let celeb = {
+					  type: "postback",
+				  	title: castData[i].name,
+				  	payload: castData[i].name
+          }
+
+					buttons.push(celeb);
+         }
+
+			 console.log('llll', message)
+            sendMessage(userId, message);
       }
-      var message = greeting + "My name is Movies Q Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      callSendAPI(sender_psid, {text: message});
-     });
-   }
-		let response;
-console.log('rrr', received_postback)
-		// Get the payload for the postback
-		// Set the response based on the postback payload
-		if(payload === 'yes') {
-			response = { "text": "Thanks!" }
-		} else if(payload === 'no') {
-			response = { "text": "Ooops, try sending another image." }
-		}
-
-		callSendAPI(sender_psid, response);
+    }
+  });
 }
 
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-
-	// Construct the message body
-	let request_body = {
-		"recipient": {
-			"id": sender_psid
-		},
-		"message": response
-	}
-
-	//Send the HTTP request to the Messenger Platform
-	request({
-		"url": "https://graph.facebook.com/v2.6/me/messages",
-		"qs": { "access_token": process.env.PAGE_ACCESS_TOKEN},
-		"method": "POST",
-		"json": request_body
-	}, (err, res, body) => {
-		if(!err) {
-			console.log('message.sent!');
-		} else {
-			console.error("Unable to send message:" + err);
-		}
-	});
-
-
-
-
-
+// sends message to user
+function sendMessage(recipientId, message) {
+	console.log('kkk', message)
+	console.log('hhh', recipientId)
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: "EAAFdfOUojVABAMka3jZAw7ydGBHZBf7gH1xqCYehq3OF3mGtndpsYahQCWPAlraDqLXMaDFIHot45EBI2SBWlWI7XbmXr1sTVNmPy0Lo7Yrkn2dbi7bZA3ySwnZAlezzVOxZAavXZBqDTF80HCW1l5b3T5Ro4Va7aaYs52IOqy9QZDZD"},
+        method: "POST",
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
 }
-
-// Creates the endpoint for Facebook webhook
-app.post('/webhook/', (req, res) => {
-
-	let body = req.body;
-
-	if(body.object === 'page') {
-
-		body.entry.forEach(function(entry) {
-console.log('entry', entry)
-
-			let webhook_event = entry.messaging[0];
-		//	console.log('webhook_event', webhook_event);
-
-
-			// Get the sender PSID
-		  let sender_psid = webhook_event.sender.id;
-		  console.log('Sender PSID: ' + sender_psid);
-
-			// Check if the event is a message or postback and
-	    // pass the event to the appropriate handler function
-			if(webhook_event.message) {
-				handleMessage(sender_psid, webhook_event.message);
-			} else if(webhook_event.postback) {
-				handlePostback(sender_psid, webhook_event.postback);
-			}
-		});
-
-		res.status(200).send('EVENT_RECEIVED');
-	} else {
-		res.sendStatus(404);
-	}
-});
-
-
-//Add an API endpoint to process messages.
-
-// app.post('/webhook/', (req,res) => {
-// 	let messagingEvents = req.body.entry[0].messaging;
-// 	for(let i = 0; i <messagingEvents.length; i++) {
-// 		let event = messagingEvents[i];
-// 		let sender = event.sender.id;
-//
-// 		if(event.message && event.message.text) {
-// 			let text = event.message.text;
-// 			console.log('text', text);
-// 			sendTextMessage(sender, "Text received, echo: " + text.substing(0, 200));
-// 		}
-// 	}
-// 	res.sendStatus(200);
-// });
-
-
-//const token = "EAAFdfOUojVABAMka3jZAw7ydGBHZBf7gH1xqCYehq3OF3mGtndpsYahQCWPAlraDqLXMaDFIHot45EBI2SBWlWI7XbmXr1sTVNmPy0Lo7Yrkn2dbi7bZA3ySwnZAlezzVOxZAavXZBqDTF80HCW1l5b3T5Ro4Va7aaYs52IOqy9QZDZD"
